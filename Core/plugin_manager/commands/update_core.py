9d416415660586d73c40d7b4dc23b85c5edbbb80
GaviTSRA <gavitsra@gmail.com>
2021-10-03 19:10:17 +0200

    plugin_manager v0.2

--- Core/plugin_manager/commands/update_core.py
+++ Core/plugin_manager/commands/update_core.py
@@ -1,4 +1,36 @@
-import requests
+import shutil
+import os
+import urllib.request
+import zipfile
 
 def update_core(*args):
-    res = requests.request("GET", "https://github.com/GaviTSRA/TSR-Console-Core")
+    installation_dir = os.getenv("APPDATA") + "\\TSR Console\\"
+    print(installation_dir)
+
+    # TODO check if an update is available
+
+    print("Starting core update...")
+    if not os.path.exists(installation_dir + "update"): os.mkdir(installation_dir + "update")
+
+    print("Downloading updated core...")
+    urllib.request.urlretrieve("https://github.com/GaviTSRA/TSR-Console-Core/archive/refs/heads/master.zip", os.getenv("APPDATA") + "\\TSR Console\\update\\core.zip")
+    print("Done")  
+
+    print("Extracting...")
+    with zipfile.ZipFile(installation_dir + "update\\core.zip", 'r') as zip_ref:
+        zip_ref.extractall(installation_dir + "update\\core\\")
+    print("Done")
+
+    print("Removing old files...")
+    shutil.rmtree(installation_dir + "Core")
+    print("Done")
+
+    print("Copying new files...")
+    shutil.move(installation_dir + "update\\core\\TSR-Console-Core-master\\Core", installation_dir)
+    print("Done")
+
+    print("Cleaning up...")
+    shutil.rmtree(installation_dir + "update")
+    print("Done")
+
+    print("Update completed")
